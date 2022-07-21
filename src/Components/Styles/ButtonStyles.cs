24646d9a5d89a2749a65484f0a5f67e7f7e5e211
CollapsedLight <>
2022-07-21 23:48:54 +0200

    feat: add extension method based component styling
    Just a concept to test the idea

---
+++ src/Components/Styles/ButtonStyles.cs
@@ -0,0 +1,32 @@
+﻿namespace CollapsedLight.WebApp.Components.Styles
+{
+    public record ButtonStyle(string Button, string Text, string TextStyle)
+    {
+        public ButtonStyle()
+            : this(string.Empty, string.Empty, string.Empty)
+        {}
+    };
+
+    public static class ClButtonStyles
+    {
+        public static ButtonStyle Secondary(this ClButtonDyn style, string Label)
+        {
+            return new ButtonStyle 
+            {
+                Button = "bg-secondary hover:drop-shadow-md text-secondary-on rounded-lg",
+                Text = Label,
+                TextStyle = "flex justify-center text-xl text-gray-300 test-center"
+            };
+        }
+
+        public static ButtonStyle Tertiary(this ClButtonDyn style, string Label)
+        {
+            return new ButtonStyle
+            {
+                Button = "bg-tertiary hover:drop-shadow-md text-tertiary-on",
+                Text = Label,
+                TextStyle = "text-lg text-gray-300"
+            };
+        }
+    }
+}
